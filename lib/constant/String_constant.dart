3719d02b2d1f509d2c2d54a580bf7c157ff7a525
清水 <noon3m.op.sh@gmail.com>
2023-08-09 13:56:07 +0900

    [add]マージ

---
+++ lib/constant/String_constant.dart
@@ -0,0 +1,5 @@
+class StringConst {
+  static const String titlePath = 'assets/images/apptitle.png';
+
+  static const String bottlePath = 'assets/images/bottle.png';
+}
---
+++ lib/model/recipe_model.dart
@@ -0,0 +1,8 @@
+class RecipeModel {
+  final int recipeId;
+  final String title;
+  final String? imagePath;
+
+  RecipeModel(
+      {required this.recipeId, required this.title, required this.imagePath});
+}
