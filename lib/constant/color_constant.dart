b1bd86c4c8d4da1b58cbe782af7ebbf1ef128aae
yutaro s <107845401+noonyuu@users.noreply.github.com>
2023-10-19 19:13:26 +0900

    Update color_constant.dart
    [fix]元のファイルのカラーコードを採用

--- lib/constant/color_constant.dart
+++ lib/constant/color_constant.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ColorConst {
-  // static const Color background = Color.fromRGBO(254, 244, 207, 1);
-  static const Color background = Color.fromRGBO(223, 215, 191, 1.0); // 変更
-  // static const Color mainColor = Color.fromRGBO(255, 200, 0, 1);
-  static const Color mainColor = Color.fromRGBO(242, 234, 211, 1.0); // 変更
+  static const Color background = Color.fromRGBO(254, 244, 207, 1);
+  // static const Color background = Color.fromRGBO(223, 215, 191, 1.0); // 変更
+  static const Color mainColor = Color.fromRGBO(255, 200, 0, 1);
+  // static const Color mainColor = Color.fromRGBO(242, 234, 211, 1.0); // 変更
   static const Color white = Color.fromRGBO(255, 255, 250, 1);
   static const Color tag = Color.fromRGBO(238, 238, 238, 1);
   static const Color black = Color.fromRGBO(10, 10, 10, 1);
