018d494413dc2e1a6271339e18e73565245b7f21
shimizu <noon3m.op.sh@gmail.com>
2023-12-02 04:31:29 +0900

    [add]レスポンシブ用コンスタントの追加

--- lib/constant/color_constant.dart
+++ lib/constant/color_constant.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ColorConst {
-  static const Color background = Color.fromRGBO(254, 244, 207, 1);
-  static const Color mainColor = Color.fromRGBO(255, 200, 0, 1);
+  static const Color background = Color(0xffFFFCF0);
+  static const Color mainColor = Color(0xffC4C9CF);
   static const Color white = Color.fromRGBO(255, 255, 250, 1);
   static const Color tag = Color.fromRGBO(238, 238, 238, 1);
   static const Color black = Color.fromRGBO(10, 10, 10, 1);
@@ -12,4 +12,5 @@ class ColorConst {
   static const Color transparent = Colors.transparent;
   static const Color paleYellow = Color.fromARGB(255, 255, 248, 183);
   static const Color red = Colors.red;
+  static const Color hintColor = Color.fromRGBO(102, 102, 102, 1);
 }
