093ad7c00b693032f6aaaf71db2a5cb81758c283
shimizu <noon3m.op.sh@gmail.com>
2023-12-02 04:21:35 +0900

    [add]レスポンシブ仕様のアップバーコンポーネント

--- lib/component/app_bar.dart
+++ lib/component/app_bar.dart
@@ -1,49 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
-import '../constant/color_constant.dart';
-import '../page/addalert.dart';
+import '../constant/layout.dart';
 
-class AppBarComponentWidget extends StatefulWidget implements PreferredSizeWidget {
-  final bool isInfoIconEnabled;
-  static String filePath = '';
+class AppBarComponentWidget extends StatelessWidget {
+  const AppBarComponentWidget({super.key, required String title, required bool isInfoIconEnabled, required BuildContext context, Color? backgroundColor});
 
-  const AppBarComponentWidget({
-    Key? key,
-    required this.isInfoIconEnabled,
-  }) : super(key: key);
-
-  @override
-  Size get preferredSize {
-    return Size(double.infinity, 96.0);
-  }
-
-  @override
-  _AppBarComponentWidgetState createState() => _AppBarComponentWidgetState();
-}
-
-class _AppBarComponentWidgetState extends State<AppBarComponentWidget> {
   @override
   Widget build(BuildContext context) {
-    return PreferredSize(
-      preferredSize: const Size.fromHeight(96.0),
-      child: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        flexibleSpace: Stack(
-          children: [
-            Positioned.fill(
-              child: Image.asset(
-                'assets/layout/appbar.png',
-                fit: BoxFit.cover,
-              ),
-              // child: SvgPicture.asset(
-              //   'assets/images/appbar.svg',
-              //   fit: BoxFit.cover,
-              // ),
+    SizeConfig sizeConfig = SizeConfig();
+    sizeConfig.init(context);
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      flexibleSpace: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/silver.jpg',
+              fit: BoxFit.fill,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
